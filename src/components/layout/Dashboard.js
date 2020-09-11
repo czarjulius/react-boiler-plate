31d7c259c64a8292bdbc670ebe30514442c3f72a
czarjulius <juliusczar.jc@gmail.com>
2020-09-11 14:57:53 +0200

    created the dashboard and get started page

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -7,10 +7,14 @@ import {
   Segment,
   Sidebar,
   Button,
+  Image,
+  Rail,
 } from "semantic-ui-react";
+import Astronaut2 from "./astronaut-2.png";
+import "./dashboard.css";
 
 const Dashboard = () => (
-  <Grid padded='vertically'>
+  <Grid divided>
     <Grid.Column width={1}>
       <Sidebar
         as={Menu}
@@ -19,7 +23,33 @@ const Dashboard = () => (
         inverted
         vertical
         visible
-        width="100%"
+      
+      >
+
+        <Menu.Item as="a">
+          <Icon name="home" />
+          Home
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="gamepad" />
+          Games
+        </Menu.Item>
+        <Menu.Item as="a">
+          <Icon name="camera" />
+          Channels
+        </Menu.Item>
+
+        
+      </Sidebar>
+    </Grid.Column>
+    <Grid.Column width={4} className="wrapperContainer">
+      {/* <Sidebar
+        as={Menu}
+        animation="push"
+        icon="labeled"
+        inverted
+        vertical
+        visible
       >
         <Menu.Item as="a">
           <Icon name="home" />
@@ -33,23 +63,45 @@ const Dashboard = () => (
           <Icon name="camera" />
           Channels
         </Menu.Item>
-      </Sidebar>
-    </Grid.Column>
-    <Grid.Column width={4}>
-      <Segment>
-        Pellentesque habitant morbi tristique senectus.
+      </Sidebar> */}
+      <Segment
+        basic
+        placeholder
+        textAlign="center"
+        style={{ height: "100vh" }}
+        vertical
+      >
+        <Rail internal  close={false}>
+          <div className="middleHeader">
+            <p>Cases</p>
+            <Icon name="users" circular />
+          </div>
+        </Rail>
+
+        <Header as="h2">Welcome!</Header>
+        <p className="welcome">
+          You can edit your account at any time by clicking your initials above.
+        </p>
       </Segment>
-      {/* <Image src="/images/wireframe/paragraph.png" /> */}
     </Grid.Column>
     <Grid.Column width={11}>
-      <Segment placeholder>
-        <Header icon>
-          <Icon name="pdf file outline" />
-          No documents are listed for this customer.
-        </Header>
-        <Button primary>Add Document</Button>
+      <Segment
+        basic
+        placeholder
+        textAlign="center"
+        style={{ height: "100vh", padding: "1em 0em", marginLeft: "80px" }}
+        vertical
+      >
+        <Image centered src={Astronaut2} size="medium" />
+        <Header as="h2">Open a New Case</Header>
+        <p className="getStarted">
+          Use our step-by-step guide to provide details about the data you are
+          collecting and its purpose. Our powerful technology and legal advisors
+          will then analyse your case and provide you with clear action steps to
+          be legally compliant.
+        </p>
+        <Button color="green">Get Started</Button>
       </Segment>
-      {/* <Image src='/images/wireframe/media-paragraph.png' /> */}
     </Grid.Column>
   </Grid>
 );
